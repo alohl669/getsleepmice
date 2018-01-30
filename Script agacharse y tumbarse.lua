dc6dbb895d6a9ec3ed015056a505b59bb4aef8ee
alohl669 <alohl669@users.noreply.github.com>
2018-01-30 17:30:45 +0100

    simplificacion
    mismo resultado
    
    Co-Authored-By: totemax <totemax@users.noreply.github.com>

--- Script agacharse y tumbarse.lua
+++ Script agacharse y tumbarse.lua
@@ -7,23 +7,24 @@ function OnEvent(event, arg)
 
 	if (event == "MOUSE_BUTTON_PRESSED" and arg == 4) then 
 		agacharse = true
+		tumbarse = false
 		tespera = 1
-	elseif (event == "MOUSE_BUTTON_RELEASED" and arg == 4) then
-		agacharse = false
-		tespera = 1
-	end
-	
-	if agacharse == true then
 		PressAndReleaseKey("c")
-		while tespera < 10 do
+		while agacharse == true and tespera < 10 do
 			Sleep(100)
 			tespera = tespera + 1
 		end
-		if tespera >= 10 then
-			tumbarse = true
-		end
-	elseif tumbarse == true then
+
+	elseif (event == "MOUSE_BUTTON_RELEASED" and arg == 4) then
+		agacharse = false
+		tumbarse = false
+		tespera = 1
+	end
+	
+	if agacharse == true and tespera >= 10 then
+		tumbarse = true
 		PressAndReleaseKey("z")
 	end
 	
 end
+cz
